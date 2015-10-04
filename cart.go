3f3e9bfc17e28ff3276661e0523d5fa92b2c7924
seu <seu@panopticon.re>
2015-10-04 23:44:01 +0200

    cart modifiable

--- cart.go
+++ cart.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"net/http"
 	"net/url"
 	"strconv"
+	"strings"
 )
 
 type CartItem struct {
-	Product Product
-	Amount  uint64
+	Product    Product
+	Amount     uint64
+	NextAmount uint64
+	PrevAmount uint64
 }
 
 func AddToCart(form url.Values, member Member, session Session, w http.ResponseWriter, r *http.Request) {
@@ -40,6 +44,37 @@ func AddToCart(form url.Values, member Member, session Session, w http.ResponseW
 	if err != nil {
 		DatabaseMutex.Unlock()
 		http.Error(w, "Failed add to cart: "+err.Error(), 500)
+		return
+	}
+
+	rows, err := tx.Query("SELECT count FROM products WHERE id = ?", id)
+	if err != nil {
+		tx.Rollback()
+		DatabaseMutex.Unlock()
+		http.Error(w, "Failed add to cart: "+err.Error(), 500)
+		return
+	}
+
+	if !rows.Next() {
+		tx.Rollback()
+		DatabaseMutex.Unlock()
+		http.Error(w, "Failed add to cart: no such cart", 500)
+		return
+	}
+
+	var avail_count uint64
+	err = rows.Scan(&avail_count)
+	if err != nil {
+		tx.Rollback()
+		DatabaseMutex.Unlock()
+		http.Error(w, "Failed add to cart: "+err.Error(), 500)
+		return
+	}
+
+	if avail_count < count {
+		tx.Rollback()
+		DatabaseMutex.Unlock()
+		http.Error(w, "Failed add to cart: no enough items in stock", 400)
 		return
 	}
 
@@ -105,7 +140,7 @@ func GetCart(member Member, session Session, w http.ResponseWriter, r *http.Requ
 
 		err := rows.Scan(&id, &name, &slug, &desc, &price, &count, &amount)
 		prod := Product{Id: id, Name: name, Slug: slug, Description: desc, Price: price, Count: count}
-		itm := CartItem{Product: prod, Amount: amount}
+		itm := CartItem{Product: prod, Amount: amount, NextAmount: amount + 1, PrevAmount: amount - 1}
 
 		if err == nil {
 			cart = append(cart, itm)
@@ -116,6 +151,217 @@ func GetCart(member Member, session Session, w http.ResponseWriter, r *http.Requ
 	DatabaseMutex.Unlock()
 
 	RenderTemplate(w, "cart", "", member, cart)
+}
+
+func PutCartItem(prodId int64, member Member, session Session, w http.ResponseWriter, r *http.Request) {
+	// Amount
+	counts, ok := r.PostForm["count"]
+	if !ok || len(counts) != 1 || len(counts[0]) == 0 {
+		http.Error(w, "Missing or empty count", 400)
+		return
+	}
+
+	count, err := strconv.ParseUint(counts[0], 10, 64)
+	if err != nil {
+		http.Error(w, "Invalid count", 400)
+		return
+	}
+
+	if count == 0 {
+		DeleteCartItem(prodId, member, session, w, r)
+		return
+	}
+
+	DatabaseMutex.Lock()
+
+	tx, err := Database.Begin()
+	if err != nil {
+		DatabaseMutex.Unlock()
+		http.Error(w, "Failed add to cart: "+err.Error(), 500)
+		return
+	}
+
+	rows, err := tx.Query("SELECT count FROM carts WHERE product = ? AND session = ?", prodId, session.Id)
+	if err != nil {
+		tx.Rollback()
+		DatabaseMutex.Unlock()
+		http.Error(w, "Failed add to cart: "+err.Error(), 500)
+		return
+	}
+
+	if !rows.Next() {
+		tx.Rollback()
+		DatabaseMutex.Unlock()
+		http.Error(w, "Failed add to cart: no such cart", 500)
+		return
+	}
+
+	var cur_count uint64
+	err = rows.Scan(&cur_count)
+	if err != nil {
+		tx.Rollback()
+		DatabaseMutex.Unlock()
+		http.Error(w, "Failed add to cart: "+err.Error(), 500)
+		return
+	}
+
+	rows, err = tx.Query("SELECT count FROM products WHERE id = ?", prodId)
+	if err != nil {
+		tx.Rollback()
+		DatabaseMutex.Unlock()
+		http.Error(w, "Failed add to cart: "+err.Error(), 500)
+		return
+	}
+
+	if !rows.Next() {
+		tx.Rollback()
+		DatabaseMutex.Unlock()
+		http.Error(w, "Failed add to cart: no such cart", 500)
+		return
+	}
+
+	var avail_count uint64
+	err = rows.Scan(&avail_count)
+	if err != nil {
+		tx.Rollback()
+		DatabaseMutex.Unlock()
+		http.Error(w, "Failed add to cart: "+err.Error(), 500)
+		return
+	}
+
+	if avail_count+cur_count < count {
+		tx.Rollback()
+		DatabaseMutex.Unlock()
+		http.Error(w, "Failed add to cart: no enough items in stock", 400)
+		return
+	}
+
+	res, err := tx.Exec("UPDATE products SET count = count + ? WHERE id = ?", cur_count, prodId)
+	if err != nil {
+		tx.Rollback()
+		DatabaseMutex.Unlock()
+		http.Error(w, "Failed add to cart: "+err.Error(), 500)
+		return
+	}
+
+	_, err = tx.Exec("UPDATE carts SET count = ? WHERE product = ? AND session = ?", count, prodId, session.Id)
+	if err != nil {
+		tx.Rollback()
+		DatabaseMutex.Unlock()
+		http.Error(w, "Failed add to cart: "+err.Error(), 500)
+		return
+	}
+
+	res, err = tx.Exec("UPDATE products SET count = count - ? WHERE id = ?", count, prodId)
+	if err != nil {
+		tx.Rollback()
+		DatabaseMutex.Unlock()
+		http.Error(w, "Failed add to cart: "+err.Error(), 500)
+		return
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		http.Error(w, "Failed add to cart: "+err.Error(), 500)
+		return
+	}
+
+	ar, err := res.RowsAffected()
+	if err != nil {
+		http.Error(w, "Failed add to cart: "+err.Error(), 500)
+		return
+	}
+
+	DatabaseMutex.Unlock()
+
+	if err != nil {
+		http.Error(w, "Failed add to cart: "+err.Error(), 500)
+		return
+	}
+
+	if ar != 1 {
+		http.Error(w, "Failed add to cart: "+err.Error(), 500)
+		return
+	}
+
+	http.Redirect(w, r, "/cart", 301)
+}
+
+func DeleteCartItem(prodId int64, member Member, session Session, w http.ResponseWriter, r *http.Request) {
+	DatabaseMutex.Lock()
+
+	tx, err := Database.Begin()
+	if err != nil {
+		DatabaseMutex.Unlock()
+		http.Error(w, "Failed add to cart: "+err.Error(), 500)
+		return
+	}
+
+	rows, err := tx.Query("SELECT count FROM carts WHERE product = ? AND session = ?", prodId, session.Id)
+	if err != nil {
+		tx.Rollback()
+		DatabaseMutex.Unlock()
+		http.Error(w, "Failed add to cart: "+err.Error(), 500)
+		return
+	}
+
+	if !rows.Next() {
+		tx.Rollback()
+		DatabaseMutex.Unlock()
+		http.Error(w, "Failed add to cart: no such cart", 500)
+		return
+	}
+
+	var cur_count uint
+	err = rows.Scan(&cur_count)
+	if err != nil {
+		tx.Rollback()
+		DatabaseMutex.Unlock()
+		http.Error(w, "Failed add to cart: "+err.Error(), 500)
+		return
+	}
+
+	res, err := tx.Exec("UPDATE products SET count = count + ? WHERE id = ?", cur_count, prodId)
+	if err != nil {
+		tx.Rollback()
+		DatabaseMutex.Unlock()
+		http.Error(w, "Failed add to cart: "+err.Error(), 500)
+		return
+	}
+
+	res, err = tx.Exec("DELETE FROM carts WHERE product = ? AND session = ?", prodId, session.Id)
+	if err != nil {
+		tx.Rollback()
+		DatabaseMutex.Unlock()
+		http.Error(w, "Failed add to cart: "+err.Error(), 500)
+		return
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		http.Error(w, "Failed add to cart: "+err.Error(), 500)
+		return
+	}
+
+	ar, err := res.RowsAffected()
+	if err != nil {
+		http.Error(w, "Failed add to cart: "+err.Error(), 500)
+		return
+	}
+
+	DatabaseMutex.Unlock()
+
+	if err != nil {
+		http.Error(w, "Failed add to cart: "+err.Error(), 500)
+		return
+	}
+
+	if ar != 1 {
+		http.Error(w, "Failed add to cart: "+err.Error(), 500)
+		return
+	}
+
+	http.Redirect(w, r, "/cart", 301)
 }
 
 func HandleCart(w http.ResponseWriter, r *http.Request) {
@@ -129,7 +375,9 @@ func HandleCart(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if r.URL.Path == "/cart" {
+	fmt.Println("HandleCart() Path = '" + r.URL.Path + "', Method = " + r.Method)
+
+	if r.URL.Path == "/cart" || r.URL.Path == "/cart/" {
 		if r.Method == "POST" {
 			err := r.ParseForm()
 
@@ -145,6 +393,38 @@ func HandleCart(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, "Method not supported", 405)
 		}
 	} else {
-		http.Error(w, "Not found", 404)
+		prodId, err := strconv.ParseInt(strings.TrimPrefix(r.URL.Path, "/cart/"), 10, 64)
+
+		if err != nil {
+			http.Error(w, "Product not found in cart: "+err.Error(), 404)
+			return
+		}
+
+		if r.Method == "POST" {
+			err := r.ParseForm()
+
+			if err != nil {
+				http.Error(w, "Failed to parse form data: "+err.Error(), 500)
+				return
+			}
+
+			var meth string
+			meths, ok := r.PostForm["_method"]
+			if !ok || len(meths) != 1 || len(meths[0]) == 0 {
+				meth = "POST"
+			} else {
+				meth = meths[0]
+			}
+
+			if meth == "PUT" {
+				PutCartItem(prodId, mem, sess, w, r)
+			} else if meth == "DELETE" {
+				DeleteCartItem(prodId, mem, sess, w, r)
+			} else {
+				http.Error(w, "Method not supported", 405)
+			}
+		} else {
+			http.Error(w, "Not found", 404)
+		}
 	}
 }
